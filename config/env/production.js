9b019966339812b3c8ed16b4fdd5f8f3b2cf1fe0
Vadim, Bochenev <bochenev.vadim@gmail.com>
2019-12-21 16:38:40 +0300

    config prod

--- config/env/production.js
+++ config/env/production.js
@@ -368,7 +368,7 @@ module.exports = {
   *                                                                         *
   ***************************************************************************/
   custom: {
-    baseUrl: 'https://example.com',
+    baseUrl: 'https://shrouded-oasis-42157.herokuapp.com',
     internalEmailAddress: 'support@example.com',
 
     // mailgunDomain: 'mg.example.com',
