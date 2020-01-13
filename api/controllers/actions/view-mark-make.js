e1de3d1e889f12cc97e746d3c823aa9426bcc160
Vadim Bochenev <vabochen@merann.ru>
2020-01-13 20:56:53 +0300

    save  data

--- api/controllers/actions/view-mark-make.js
+++ api/controllers/actions/view-mark-make.js
@@ -20,20 +20,33 @@ module.exports = {
 
     let businessPlace = {};
     const marks = await Mark.find();
-    // Respond with view.
+
+    const isTeacher = this.req.me.role ? (this.req.me.role.id === 2) : false;
+    const isStudent = this.req.me.role ? (this.req.me.role.id === 1) : false;
+
     if (this.req.me.businessPlace) {
       businessPlace = await BusinessPlace.findOne({id: this.req.me.businessPlace.id})
         .populate('subjects')
+        .populate('groups')
         .populate('users');
     }
 
     if (businessPlace.users) {
       businessPlace.users = businessPlace.users.filter((item) => item.id !== this.req.me.id);
     }
+
+
+    if(isStudent) {
+
+    }
+
+
+
     return {
       businessPlace,
       marks,
-      subject: 'Test',
+      isTeacher,
+      isStudent,
       isSuperAdmin: this.req.me.isSuperAdmin
     };
 
